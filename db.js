79b31fc3dfb0788f4157c7e83ce49e20d69441f4
rutus <rutus@phoenix.tech>
2025-12-08 09:25:03 -0600

    added db schema

---
+++ db.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const userSchema = new Schema({
+    email: {type: String, unique: true},
+    password : String,
+    firstName : String,
+    lastName: String
+});
+
+const adminSchema = new Schema({
+    email: {type: String, unique: true},
+    password : String,
+    firstName : String,
+    lastName: String
+});
+
+const courseSchema = new Schema({
+    courseTitle : String,
+    courseDescription : String,
+    price : Number,
+    imageURL : String,
+    creatorId : ObjectId
+});
+
+const purchaseSchema = new Schema({
+    userId : ObjectId,
+    creatorId : ObjectId
+});
+
+const userModel = mongoose.model('user',userSchema);
+const adminModel = mongoose.model('admin',adminSchema);
+const courseModel = mongoose.model('course',courseSchema);
+const purchaseModel = mongoose.model('purchase',purchaseSchema);
+
+module.exports = {
+    userModel: userModel,
+    adminModel: adminModel,
+    courseModel: courseModel,
+    purchaseModel: purchaseModel
+}
